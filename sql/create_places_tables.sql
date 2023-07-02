9523e3c5b262512042509be2cb9e6c064a598d26
Ben Hancock <ben@benghancock.com>
2023-07-01 17:25:48 -0700

    write ddl for places tables

--- sql/create_places_tables.sql
+++ sql/create_places_tables.sql
@@ -1,16 +1,30 @@
+-- Data definition statements for various "places" datasets from Pleiades
+
 -- Create the general "places" table for the Pleiades data
 CREATE TABLE IF NOT EXISTS places (
-	created timestamp with time zone,
-	description text,
-	details text,
-	provenance varchar(500),
-	title varchar(500),
-	uri varchar(1000),
-	id bigint,
-	representative_latitude decimal,
-	representative_longitude decimal,
-	bounding_box_wkt text
+    created     timestamp with time zone,
+    description text,
+    details     text,
+    provenance  varchar(500),
+    title       varchar(500),
+    uri         varchar(1000),
+    id          bigint,
+    representative_latitude     decimal,
+    representative_longitude    decimal,
+    bounding_box_wkt    text,
+    CONSTRAINT place_id PRIMARY KEY (id)
 );
 
--- TODO! Create place_types
--- TODO! Create places_place_types
+-- Create a table for the various place types
+CREATE TABLE IF NOT EXISTS places_types (
+    key         varchar(50),
+    term        text,
+    definition  text,
+    CONSTRAINT place_type_key PRIMARY KEY (key)
+);
+
+-- Create a fact table to join places to their place type definitions
+CREATE TABLE IF NOT EXISTS places_place_types (
+    place_id    bigint      REFERENCES places (id),
+    place_type  varchar(50) REFERENCES places_types (key)
+);
