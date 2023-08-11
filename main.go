891470e6706d22e2c6c1801269aee897f784ce38
Ben Hancock <ben@benghancock.com>
2023-08-11 05:54:52 -0700

    implement search endpoint with echo (working state)

--- main.go
+++ main.go
@@ -7,24 +7,24 @@ import (
 	"fmt"
 	_ "github.com/lib/pq"
 	"log"
+	"net/http"
 	"os"
-    "net/http"
 
-    "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 )
 
 var db *sql.DB
 
 type SearchResult struct {
-	Count	int	`json:count`
-	Results	[]PleiadesPlace `json:results`
+	Count   int             `json:count`
+	Results []PleiadesPlace `json:results`
 }
 
 type PleiadesPlace struct {
-	Name	string	`json:name`
-	Country	string	`json:country`
-	PlaceType	string	`json:placeType`
-	Description	string	`json:description`
+	Name        string `json:name`
+	Country     string `json:country`
+	PlaceType   string `json:placeType`
+	Description string `json:description`
 }
 
 func main() {
@@ -33,63 +33,38 @@ func main() {
 	dsn := fmt.Sprintf(
 		"user=%s password=%s dbname=archaia sslmode=disable\n",
 		dbUser,
-		dbPass)
+		dbPass,
+	)
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
-	}
-	log.Println("Connected to archaia!")
+	db.SetConnMaxLifetime(0)
+	db.SetMaxIdleConns(50)
+	db.SetMaxOpenConns(50)
 
 	e := echo.New()
-	e.GET("/search", searchPlaces)
+	e.GET("/search", func(c echo.Context) error {
+		result := new(SearchResult)
+		country := c.QueryParam("country")
+		places := queryCountryPlaces(db, country)
+		result.Count = len(places)
+		result.Results = places
+		return c.JSON(http.StatusOK, result)
+	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func searchPlaces(c echo.Context) error {
-	result := new(SearchResult)
-	country := c.QueryParam("country")
-	places := queryCountryPlaces(db, country)
-	result.Count = len(places)
-	result.Results = places
-	return c.JSON(http.StatusOK, result)
-}
-
-
-func queryCountryCounts(db *sql.DB) map[string]int {
-	var counts = make(map[string]int)
-	q := `
-		SELECT
-			COALESCE(country_name, '(unknown)'),
-			COUNT(DISTINCT(place_id)) AS place_count
-		FROM countries_places
-		GROUP BY country_name;
-`
-	rows, err := db.Query(q)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for rows.Next() {
-		var countryName string
-		var placeCount int
-		if err := rows.Scan(&countryName, &placeCount); err != nil {
-			log.Fatal(err)
-		}
-		counts[countryName] = placeCount
-	}
-	return counts
-}
-
 // queryCountryPlaces returns a slice of matching PleiadesPlaces
-func queryCountryPlaces (db *sql.DB, name string) []PleiadesPlace {
+func queryCountryPlaces(db *sql.DB, name string) []PleiadesPlace {
 	var matchPlaces []PleiadesPlace
 	q := `
-		SELECT place_name, country_name, place_type, descrip
+		SELECT
+			place_name,
+			country_name,
+			place_type,
+			COALESCE(descrip, '')
 		FROM countries_places
 		WHERE country_name ILIKE '%' || $1 || '%';
 	`
@@ -99,7 +74,7 @@ func queryCountryPlaces (db *sql.DB, name string) []PleiadesPlace {
 	}
 	for rows.Next() {
 		var place PleiadesPlace
-		if err := rows.Scan(&place.name, &place.country, &place.placeType, &place.description); err != nil {
+		if err := rows.Scan(&place.Name, &place.Country, &place.PlaceType, &place.Description); err != nil {
 			log.Fatal(err)
 		}
 		matchPlaces = append(matchPlaces, place)
